37dfe006f56311c9cd907a68e500313ab4177d00
liljdot <jehuoki@gmail.com>
2025-04-17 19:12:35 +0100

    Update BarSection and Gallery components for improved layout and styling

--- src/app/components/home/BarSection.tsx
+++ src/app/components/home/BarSection.tsx
@@ -1,18 +1,17 @@
 import { openSans } from "@/app/layout";
-import Image, { StaticImageData } from "next/image";
 import Gallery from "./Gallery";
 
 const BarSection: React.FC = () => {
 
     return (
         <>
-            <section className="bg-neutral flex flex-col gap-6 py-15 xl:py-45">
-                <div className="flex flex-col gap-2.5 md:gap-8 lg:gap-12 xl:gap-7.5">
+            <section className="bg-neutral flex flex-col gap-6 md:gap-7 lg:gap-8 py-15 md:py-24 lg:py-36 xl:py-45">
+                <div className="flex flex-col gap-2.5 md:gap-4 lg:gap-6 xl:gap-7.5">
                     <h3 className="text-primary text-center self-center text-xl md:text-3xl lg:text-5xl xl:text-7xl font-semibold xl:font-medium opacity-80">
                         OUR BAR
                     </h3>
 
-                    <p className={"text-center text-sm/6 md:text-base/8 xl:font-semibold text-base-content grow-0 px-12 md:px-10 lg:px-20 xl:px-32 " + openSans.className}>
+                    <p className={"text-center text-sm/6 md:text-base/8 text-base-content grow-0 px-12 md:px-20 lg:px-32 xl:px-47.5 " + openSans.className}>
                         Our bar offers a carefully curated selection of wines to suit every preference. Enjoy bold and velvety reds, crisp and refreshing whites, elegant rosés, and rich, bubbly champagnes. Whether you prefer dry, sweet, or something in between, we have the perfect pour for every occasion.
                     </p>
                 </div>
--- src/app/components/home/Gallery.tsx
+++ src/app/components/home/Gallery.tsx
@@ -5,7 +5,7 @@ import Image, { StaticImageData } from "next/image"
 const GalleryImage: React.FC<{ image: StaticImageData }> = ({ image }) => {
 
     return (
-        <figure className="relative w-52.5 h-47 shrink-0">
+        <figure className="relative w-52.5 h-47 md:size-full shrink-0 hover:scale-150 hover:transition-transform duration-300 ease-in-out hover:z-1">
             <Image fill src={image} alt="bar image" />
         </figure>
     )
@@ -17,7 +17,7 @@ const Gallery: React.FC = () => {
 
     return (
         <>
-            <div className="flex flex-row gap-2.5 overflow-x-scroll">
+            <div className="flex flex-row md:grid grid-cols-3 xl:grid-cols-[repeat(3,_370px)] md:auto-rows-[200px] lg:auto-rows-[330px] xl:auto-rows-[358px] gap-2.5 md:gap-4 xl:gap-5 overflow-x-scroll md:overflow-visible justify-center">
                 {
                     images.map((image: any, index: number) => <GalleryImage key={"bar-image-" + index} image={image} />)
                 }
